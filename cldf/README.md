d65f6eacf5aa601de24bd774381664fdbdecdaaf
zifan <j22melody@gmail.com>
2021-10-27 16:30:34 +0200

    CLDF conversion

--- cldf/README.md
+++ cldf/README.md
@@ -1,3 +1,128 @@
-# CLDF directory
+<a name="ds-structuredatasetmetadatajson"> </a>
 
-This directory contains the dataset formatted as [CLDF dataset](https://cldf.clld.org).
+# StructureDataset 100LC
+
+**CLDF Metadata**: [StructureDataset-metadata.json](./StructureDataset-metadata.json)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF StructureDataset](http://cldf.clld.org/v1.0/terms.rdf#StructureDataset)
+[dc:identifier](http://purl.org/dc/terms/identifier) | https://github.com/100LC/100LC
+[dcat:accessURL](http://www.w3.org/ns/dcat#accessURL) | git@github.com:cldf-datasets/100LC
+[prov:wasDerivedFrom](http://www.w3.org/ns/prov#wasDerivedFrom) | <ol><li><a href="git@github.com:cldf-datasets/100LC/tree/2abbe8c">git@github.com:cldf-datasets/100LC 2abbe8c</a></li><li><a href="git@github.com:glottolog/glottolog/tree/f6531fcec3">Glottolog v4.4-20-gf6531fcec3</a></li><li><a href="git@github.com:100LC/100LC/tree/37fd17a54">git@github.com:100LC/100LC 37fd17a54</a></li></ol>
+[prov:wasGeneratedBy](http://www.w3.org/ns/prov#wasGeneratedBy) | <ol><li><strong>python</strong>: 3.8.11</li><li><strong>python-packages</strong>: <a href="./requirements.txt">requirements.txt</a></li></ol>
+[rdf:ID](http://www.w3.org/1999/02/22-rdf-syntax-ns#ID) | 100LC
+[rdf:type](http://www.w3.org/1999/02/22-rdf-syntax-ns#type) | http://www.w3.org/ns/dcat#Distribution
+
+
+## <a name="table-valuescsv"></a>Table [values.csv](./values.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF ValueTable](http://cldf.clld.org/v1.0/terms.rdf#ValueTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 80080
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Language_ID](http://cldf.clld.org/v1.0/terms.rdf#languageReference) | `string` | References [languages.csv::ID](#table-languagescsv)
+[Parameter_ID](http://cldf.clld.org/v1.0/terms.rdf#parameterReference) | `string` | References [parameters.csv::ID](#table-parameterscsv)
+[Value](http://cldf.clld.org/v1.0/terms.rdf#value) | `string` | 
+[Comment](http://cldf.clld.org/v1.0/terms.rdf#comment) | `string` | 
+[Corpus_ID](http://cldf.clld.org/v1.0/terms.rdf#contributionReference) | `string` | References [contributions.csv::ID](#table-contributionscsv)
+`text_raw` | `string` | 
+`label` | `string` | 
+`text` | `string` | 
+`translation` | `string` | 
+`glossing` | `string` | 
+`segmentation` | `string` | 
+`phonological` | `string` | 
+`morphomic` | `string` | 
+`footnote` | `string` | 
+
+## <a name="table-parameterscsv"></a>Table [parameters.csv](./parameters.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF ParameterTable](http://cldf.clld.org/v1.0/terms.rdf#ParameterTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 20
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Name](http://cldf.clld.org/v1.0/terms.rdf#name) | `string` | 
+[Corpus_ID](http://cldf.clld.org/v1.0/terms.rdf#contributionReference) | `string` | References [contributions.csv::ID](#table-contributionscsv)
+`language_name_wals` | `string` | 
+`language_name_glotto` | `string` | 
+`iso639_3` | `string` | 
+`year_composed` | `string` | 
+`year_published` | `string` | 
+`mode` | `string` | 
+`genre_broad` | `string` | 
+`genre_narrow` | `string` | 
+`writing_system` | `string` | 
+`special_characters` | `string` | 
+`short_description` | `string` | 
+`source` | `string` | 
+`copyright_short` | `string` | 
+`copyright_long` | `string` | 
+`sample_type` | `string` | 
+`comments` | `string` | 
+
+## <a name="table-contributionscsv"></a>Table [contributions.csv](./contributions.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF ContributionTable](http://cldf.clld.org/v1.0/terms.rdf#ContributionTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 17
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Name](http://cldf.clld.org/v1.0/terms.rdf#name) | `string` | 
+[Language_ID](http://cldf.clld.org/v1.0/terms.rdf#languageReference) | `string` | References [languages.csv::ID](#table-languagescsv)
+`genre_broad` | `string` | 
+`mode` | `string` | 
+
+## <a name="table-languagescsv"></a>Table [languages.csv](./languages.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF LanguageTable](http://cldf.clld.org/v1.0/terms.rdf#LanguageTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 100
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Name](http://cldf.clld.org/v1.0/terms.rdf#name) | `string` | 
+[Macroarea](http://cldf.clld.org/v1.0/terms.rdf#macroarea) | `string` | 
+[Latitude](http://cldf.clld.org/v1.0/terms.rdf#latitude) | `decimal` | 
+[Longitude](http://cldf.clld.org/v1.0/terms.rdf#longitude) | `decimal` | 
+[Glottocode](http://cldf.clld.org/v1.0/terms.rdf#glottocode) | `string` | 
+[ISO639P3code](http://cldf.clld.org/v1.0/terms.rdf#iso639P3code) | `string` | 
+`wals_code` | `string` | 
+`name_glotto` | `string` | 
+`name_wals` | `string` | 
+`level` | `string` | 
+`status` | `string` | 
+`family_id` | `string` | 
+`top_level_family` | `string` | 
+`genus_wals` | `string` | 
+`family_wals` | `string` | 
+`macroarea_wals` | `string` | 
+`latitude_wals` | `string` | 
+`longitude_wals` | `string` | 
+`folder_language_name` | `string` | 
+
--- cldfbench_100LC.py
+++ cldfbench_100LC.py
@@ -1,26 +1,137 @@
 import pathlib
-
+import subprocess
+import hashlib
 from cldfbench import Dataset as BaseDataset
+from cldfbench import CLDFSpec
 
 
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "100LC"
 
+    valueTableProperties = ['text_raw', 'label', 'text', 'translation', 'glossing', 'segmentation', 'phonological', 'morphomic', 'footnote']
+    parameterTableProperties = ['language_name_wals', 'language_name_glotto', 'iso639_3', 'year_composed', 'year_published', 'mode', 'genre_broad', 'genre_narrow', 'writing_system', 'special_characters', 'short_description', 'source', 'copyright_short', 'copyright_long', 'sample_type', 'comments']
+    contributionTableProperties = ['genre_broad', 'mode']
+    languageTableProperties = ['wals_code', 'name_glotto', 'name_wals', 'level', 'status', 'family_id', 'top_level_family', 'genus_wals', 'family_wals', 'macroarea_wals', 'latitude_wals', 'longitude_wals', 'folder_language_name']
+
     def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
-        return super().cldf_specs()
+        return CLDFSpec(dir=self.cldf_dir, module='StructureDataset')
 
     def cmd_download(self, args):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw_dir`, e.g.
+        subprocess.check_call('git -C {} submodule update --remote'.format(self.dir.resolve()), shell=True)
+        subprocess.check_call('cd ./raw/100LC/Database/ && Rscript ./to_csv.R', shell=True)
 
-        >>> self.raw_dir.download(url, fname)
-        """
-        pass
+    def create_schema(self, ds):
+        # values.csv
+        ds.remove_columns('ValueTable', 'Code_ID', 'Source')
+        ds.add_columns(
+            'ValueTable',
+            {
+                "dc:extent": "singlevalued",
+                "datatype": "string",
+                "propertyUrl": "http://cldf.clld.org/v1.0/terms.rdf#contributionReference",
+                "required": True,
+                "name": "Corpus_ID"
+            },
+            *self.valueTableProperties,
+        )
+
+        # parameters.csv
+        ds.add_component('ParameterTable')
+        ds.remove_columns('ParameterTable', 'Description')
+        ds.add_columns(
+            'ParameterTable',
+            {
+                "dc:extent": "singlevalued",
+                "datatype": "string",
+                "propertyUrl": "http://cldf.clld.org/v1.0/terms.rdf#contributionReference",
+                "required": True,
+                "name": "Corpus_ID"
+            },
+            *self.parameterTableProperties,
+        )
+
+        # contributions.csv
+        ds.add_component('ContributionTable')
+        ds.remove_columns('ContributionTable', 'Description', 'Contributor', 'Citation')
+        ds.add_columns(
+            'ContributionTable',
+            {'name': 'Language_ID', 'propertyUrl': "http://cldf.clld.org/v1.0/terms.rdf#languageReference"},
+            *self.contributionTableProperties,
+        )
+
+        # languages.csv
+        ds.add_component('LanguageTable', *self.languageTableProperties)
+
+        ds.add_foreign_key('ValueTable', 'Corpus_ID', 'ContributionTable', 'ID')
+        ds.add_foreign_key('ParameterTable', 'Corpus_ID', 'ContributionTable', 'ID')
+        ds.add_foreign_key('ContributionTable', 'Language_ID', 'LanguageTable', 'ID')
 
     def cmd_makecldf(self, args):
-        """
-        Convert the raw data to a CLDF dataset.
+        self.create_schema(args.writer.cldf)
+            
+        # languages.csv
+        for row in self.raw_dir.read_csv(
+            self.raw_dir / '100LC' / 'Database' / 'language.csv',
+            dicts=True,
+        ):
+            args.writer.objects['LanguageTable'].append({
+                'ID': row['id'],
+                'Name': row['name'],
+                'Glottocode': row['glottocode'],
+                'ISO639P3code': row['iso639_3'],
+                'Macroarea': row['macroarea_glotto'],
+                'Latitude': row['latitude_glotto'],
+                'Longitude': row['longitude_glotto'],
+                **{ k: row[k] for k in self.languageTableProperties}
+            })
 
-        >>> args.writer.objects['LanguageTable'].append(...)
-        """
+        # contributions.csv
+        for row in self.raw_dir.read_csv(
+            self.raw_dir / '100LC' / 'Database' / 'corpus.csv',
+            dicts=True,
+        ):
+            args.writer.objects['ContributionTable'].append({
+                'ID': row['id'],
+                'Name': row['name'],
+                'Language_ID': row['language_id'],
+                **{ k: row[k] for k in self.contributionTableProperties}
+            })
+
+        # parameters.csv
+        for row in self.raw_dir.read_csv(
+            self.raw_dir / '100LC' / 'Database' / 'file.csv',
+            dicts=True,
+        ):
+            current_corpus = None
+            for corpus in args.writer.objects['ContributionTable']:
+                if corpus['ID'] == row['corpus_id']:
+                    current_corpus = corpus
+
+            args.writer.objects['ParameterTable'].append({
+                'ID': row['id'],
+                'Name': row['filename'],
+                'Corpus_ID': row['corpus_id'],
+                'Language_ID': current_corpus['Language_ID'],
+                **{ k: row[k] for k in self.parameterTableProperties}
+            })
+
+        # values.csv
+        for row in self.raw_dir.read_csv(
+            self.raw_dir / '100LC' / 'Database' / 'line.csv',
+            dicts=True,
+        ):
+            current_file = None
+            for file in args.writer.objects['ParameterTable']:
+                if file['ID'] == row['file_id']:
+                    current_file = file
+
+            args.writer.objects['ValueTable'].append({
+                'ID': row['id'],
+                'Value': row['text'],
+                'Parameter_ID': row['file_id'],
+                'Corpus_ID': current_file['Corpus_ID'],
+                'Language_ID': current_file['Language_ID'],
+                'Comment': row['comment'],
+                **{ k: row[k] for k in self.valueTableProperties}
+            })
